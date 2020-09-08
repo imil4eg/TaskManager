77ac92e94734019116354256d115b98192ff90c3
Emil Sharkhemullin (Akvelon INC) <v-emsha@microsoft.com>
2020-09-08 11:46:13 +0300

    Added Domain project and implemented entities

---
+++ TaskManager/TaskManager.Entities/Entities/ProjectEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System.Collections.Generic;
+
+namespace TaskManager.Domain
+{
+    public sealed class ProjectEntity : TableEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public IReadOnlyCollection<TaskEntity> Tasks { get; set; } 
+    }
+}
---
+++ TaskManager/TaskManager.Entities/Entities/TaskEntity.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+
+namespace TaskManager.Domain
+{
+    public sealed class TaskEntity : TableEntity
+    {
+        public int Id { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
